1f355979e5d847b8259f8df6faae4a6d8256fed4
Rafael Salcedo <rafaelsava@unisabana.edu.co>
2024-09-21 14:25:48 -0500

    add animation

--- java.js
+++ java.js
@@ -2,6 +2,7 @@
 onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
+    document.getElementsByClassName("loader").classList.remove("not-loaded");
     clearTimeout(c);
   }, 1000);
 };
