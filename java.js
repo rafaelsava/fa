e8aa32e1e478cf9de115181c57c1c45ebc6ccbf7
Rafael Salcedo <rafaelsava@unisabana.edu.co>
2024-09-21 14:26:04 -0500

    change java

--- java.js
+++ java.js
@@ -2,7 +2,6 @@
 onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
-    document.getElementsByClassName("loader").classList.remove("not-loaded");
     clearTimeout(c);
   }, 1000);
 };
